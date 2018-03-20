63a785291246301193ea09346454b88b5bc7ccc9
Saurabh Singh <sausing@microsoft.com>
2018-03-20 00:15:40 -0700

    Parameterize things

--- sqlprelogin/Program.cs
+++ sqlprelogin/Program.cs
@@ -3,14 +3,26 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace sqlprelogin
 {
     class Program
     {
+        private static string hostName = null;
+        private static int port = 1433;
+
+        private static int threadCount = 10;
+
         static void Main(string[] args)
         {
-            int runCount = 100;
+            if(!parseArgs(args))
+            {
+                return;
+            }
+            WriteLine($"Executing for {hostName} {port} {threadCount}");
+
+            int runCount = threadCount;
             long[] timetaken = new long[runCount];
             Task[] tasks = new Task[runCount];
 
@@ -18,10 +30,13 @@ namespace sqlprelogin
             {
                 Task t = Task.Factory.StartNew((ctr) =>
                 {
-                    SQLConnection connection = new SQLConnection("ss-desktop2", 1433);
+                    SQLConnection connection = new SQLConnection(hostName, port);
+
                     long sslHandShakeTime = connection.Connect();
+
                     timetaken[(int)ctr] = sslHandShakeTime;
                 }, i);
+
                 tasks[i] = t;
             }
 
@@ -32,9 +47,33 @@ namespace sqlprelogin
             {
                 sum += timetaken[i];
             }
-            Console.WriteLine(sum / runCount);
+
+            WriteLine($"{sum / runCount} milliseconds per execution for SSL Handshake for {threadCount} threads ");
         }
 
+        private static bool parseArgs(string[] args)
+        {
+            if(args == null || args.Length < 3)
+            {
+                WriteLine("Usage dotnet run --framework <F/W> <hostname> <port> <threadCount>");
+                return false;
+            }
+
+            hostName = args[0];
+            if(!int.TryParse(args[1], out port))
+            {
+                WriteLine("Usage dotnet run --framework <F/W> <hostname> <port> <threadCount>");
+                return false;
+            }
+            if (!int.TryParse(args[2], out threadCount))
+            {
+                WriteLine("Usage dotnet run --framework <F/W> <hostname> <port> <threadCount>");
+                return false;
+            }
+
+            return true;
+
+        }
     }
 
 }
